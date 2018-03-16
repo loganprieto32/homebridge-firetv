2d302c118e0c7f183b859ab1b6bd9e8e2fa6e532
loganprieto32 <37426389+loganprieto32@users.noreply.github.com>
2018-03-15 20:02:29 -0600

    Create index.js
    added research links

---
+++ index.js
@@ -0,0 +1,4 @@
+http://blog.theodo.fr/2017/08/make-siri-perfect-home-companion-devices-not-supported-apple-homekit/
+
+https://github.com/happyleavesaoc/python-firetv
+https://developer.amazon.com/docs/fire-tv/connecting-adb-to-device.html
